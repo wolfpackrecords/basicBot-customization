b6f370620c5c2fcd120888f6248bb5112a7b921a
wolfpackrecords <martinkotkov@gmail.com>
2014-09-21 15:43:41 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -40,7 +40,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Bacon!!!");
+                    API.sendChat("/me ate some bacon!");
                 }
             }
         };
