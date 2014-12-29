bc93f7498a78af420ea9058a4c55008820ad00ff
wolfpackrecords <martinkotkov@gmail.com>
2014-12-29 19:56:05 +0000

    Update extension.js

--- extension.js
+++ extension.js
@@ -32,7 +32,7 @@
  
          */
  
-        bot.commands.baconCommand = {
+  bot.commands.baconCommand = {
             command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
@@ -40,10 +40,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me BACON!");
-                }
-            }
-        };
+                    API.sendChat("me/ Bacon! ");
                
         bot.commands.testCommand = {
                 command: 'test',
