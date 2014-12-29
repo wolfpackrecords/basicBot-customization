84d9bdab19a15b2ed54dfbb96f612dd4feabfebe
wolfpackrecords <martinkotkov@gmail.com>
2014-12-29 19:47:14 +0000

    Update extension.js

--- extension.js
+++ extension.js
@@ -40,7 +40,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat(Hi @" + chat.un + " , this is a test"");
+                    API.sendChat(/me Hi @" + chat.un + " , this is a test"");
                 }
             }
         };
@@ -53,7 +53,7 @@
                         if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                         if (!bot.commands.executable(this.rank, chat)) return void (0);
                         else {
-                                API.sendChat("Here @" + chat.un + " ,is a piece of bacon");
+                                API.sendChat(/me "Here @" + chat.un + " ,is a piece of bacon");
                         }
                 }
         };
